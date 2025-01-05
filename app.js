2f22d0075444b16a1a237c6fc8c5b7fd08fd5115
dxthki <thinh.kieu@tuni.fi>
2025-01-05 16:23:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+// Fallback to index.html for single-page applications
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
