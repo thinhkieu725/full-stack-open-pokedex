56895a9be46c67b6c363f06e45cc8940948a23c8
dxthki <thinh.kieu@tuni.fi>
2025-01-05 19:40:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('*', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html')
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
